2270cbbb81a49986720bf1e616c1a82ee9085b00
JaehyunLee <ljh@JaehyunLees-MacBook-Pro.local>
2025-05-27 13:43:41 +0900

    feat: 16px icons (#3) Facebook, Instagram, Tiktok and Twitter icon

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,16 @@
+import Facebook16 from './components/icon/16px/Facebook16';
+import Instagram16 from './components/icon/16px/Instagram16';
+import Ticktok16 from './components/icon/16px/Tiktok16';
+import Twitter16 from './components/icon/16px/Twitter16';
 function App() {
-  return <></>;
+  return (
+    <div className="flex flex-col justify-center items-center bg-black gap-2">
+      <Twitter16 />
+      <Facebook16 />
+      <Ticktok16 />
+      <Instagram16 />
+    </div>
+  );
 }
 
 export default App;
