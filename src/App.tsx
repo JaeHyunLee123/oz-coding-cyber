84b074236e4e8fa97aa3faf5e1f44c90b061cc89
JaehyunLee <ljh@JaehyunLees-MacBook-Pro.local>
2025-05-27 14:29:28 +0900

    feat: 24px icons (#3)

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,59 @@
-import Facebook16 from './components/icon/16px/Facebook16';
-import Instagram16 from './components/icon/16px/Instagram16';
-import Ticktok16 from './components/icon/16px/Tiktok16';
-import Twitter16 from './components/icon/16px/Twitter16';
+import Arrow24 from './components/icon/24px/Arrow24';
+import ArrowDown24 from './components/icon/24px/ArrowDown24';
+import BatteryCapcity24 from './components/icon/24px/BatteryCapcity24';
+import Camera24 from './components/icon/24px/Camera24';
+import ChevronDown24 from './components/icon/24px/ChevronDown24';
+import Close24 from './components/icon/24px/Close24';
+import Computer24 from './components/icon/24px/Computer24';
+import CPU24 from './components/icon/24px/CPU24';
+import Edit24 from './components/icon/24px/Edit24';
+import Filters24 from './components/icon/24px/Filters24';
+import FrontCamera24 from './components/icon/24px/FrontCamera24';
+import Gaming24 from './components/icon/24px/Gaming24';
+import Headphone24 from './components/icon/24px/Headphone24';
+import Location24 from './components/icon/24px/Location24';
+import MainCamera24 from './components/icon/24px/MainCamera24';
+import NoEdit24 from './components/icon/24px/NoEdit24';
+import NumberOfCores24 from './components/icon/24px/NumberOfCores24';
+import Payment24 from './components/icon/24px/Payment24';
+import Phone24 from './components/icon/24px/Phone24';
+import Plus24 from './components/icon/24px/Plus24';
+import ScreenSize24 from './components/icon/24px/ScreenSize24';
+import Search24 from './components/icon/24px/Search24';
+import Shipping24 from './components/icon/24px/Shipping24';
+import SmartWatch24 from './components/icon/24px/SmartWatch24';
+import ToEdit24 from './components/icon/24px/ToEdit24';
+import View24 from './components/icon/24px/View24';
+
 function App() {
   return (
-    <div className="flex flex-col justify-center items-center bg-black gap-2">
-      <Twitter16 />
-      <Facebook16 />
-      <Ticktok16 />
-      <Instagram16 />
+    <div className="flex flex-col justify-center items-center bg-green-200 gap-2">
+      <Arrow24 />
+      <ArrowDown24 />
+      <BatteryCapcity24 />
+      <Camera24 />
+      <ChevronDown24 />
+      <Close24 />
+      <Computer24 />
+      <CPU24 />
+      <Edit24 />
+      <Filters24 />
+      <FrontCamera24 />
+      <Gaming24 />
+      <Headphone24 />
+      <Location24 />
+      <MainCamera24 />
+      <NoEdit24 />
+      <NumberOfCores24 />
+      <Payment24 />
+      <Phone24 />
+      <Plus24 />
+      <ScreenSize24 />
+      <Search24 />
+      <Shipping24 />
+      <SmartWatch24 />
+      <ToEdit24 />
+      <View24 />
     </div>
   );
 }
---
+++ src/components/icon/24px/ArrowDown24.tsx
@@ -0,0 +1,7 @@
+export default function ArrowDown24() {
+  return (
+    <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+      <path d="M18 9L12 15L6 9" stroke="#191919" stroke-width="1.5" stroke-linecap="round" />
+    </svg>
+  );
+}
