eb90c2f35fa1f1b4389c8e03a03f114913f16fd1
JaehyunLee <ljh@JaehyunLees-MacBook-Pro.local>
2025-05-29 15:11:13 +0900

    feat: 56pixel icons (#3) Delivery, Guaranteed and Stock icon

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,5 @@
-import Camera48 from './components/icon/48px/Camera48';
-import Computer48 from './components/icon/48px/Computer48';
-import Gaming48 from './components/icon/48px/Gaming48';
-import Headphone48 from './components/icon/48px/Headphone48';
-import Phone48 from './components/icon/48px/Phone48';
-import SmartWatch48 from './components/icon/48px/SmartWatch48';
-
 function App() {
-  return (
-    <div className="flex flex-col justify-center items-center bg-green-200 gap-2">
-      <Camera48 />
-      <Computer48 />
-      <Gaming48 />
-      <Headphone48 />
-      <Phone48 />
-      <SmartWatch48 />
-    </div>
-  );
+  return <div></div>;
 }
 
 export default App;
